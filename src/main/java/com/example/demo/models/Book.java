2df18d943132baeb47993182721eb780f561527f
Mersad Dela <developer78991@gmail.com>
2020-11-18 21:04:22 +0100

    java

--- src/main/java/com/example/demo/models/Book.java
+++ src/main/java/com/example/demo/models/Book.java
@@ -15,7 +15,7 @@ public class Book implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
-    private Integer id;
+    private Long id;
 
     @Size(max = 45)
     private String title;
@@ -32,11 +32,11 @@ public class Book implements Serializable {
     public Book() {
     }
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
